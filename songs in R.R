a83fa98efa80c9d5befb18ff77cdfc62e4957340
Rutvij <44467789+RutvijBhutaiya@users.noreply.github.com>
2020-02-22 10:05:10 +0530

    R Code

---
+++ songs in R.R
@@ -0,0 +1,145 @@
+
+### POPULARITY OF SONGS ###
+
+songs = read.csv('Data_train.csv')
+
+# Data Structure
+
+summary(songs)
+
+str(songs)
+head(songs)
+
+## Convert likes and popularity to Numeric
+
+songs$Likes = as.numeric(songs$Likes)
+songs$Popularity = as.numeric(songs$Popularity)
+
+
+
+# Missing Values
+
+colSums(is.na(songs))
+
+
+
+### EXPLORATERY DATA ANALYSIS ###
+
+
+# Identify Uniques values in data
+# UNiques entries : Name, Genre
+
+unique_name = unique(songs$Name)
+length(unique_name)
+
+# Timestamp Feature Expantion
+## Create Seperate Feature for Day, Month, Year, Hour, Min, Sec 
+
+songs$Day_Stamp = format(as.POSIXct(strptime(songs$Timestamp, "%Y-%m-%d %H:%M:%S", tz="")), 
+                   format = "%d")
+
+songs$Month_Stamp = format(as.POSIXct(strptime(songs$Timestamp, "%Y-%m-%d %H:%M:%S", tz="")), 
+                         format = "%m")
+
+songs$Year_Stamp = format(as.POSIXct(strptime(songs$Timestamp, "%Y-%m-%d %H:%M:%S", tz="")), 
+                         format = "%Y")
+
+songs$Hour_Stamp = format(as.POSIXct(strptime(songs$Timestamp, "%Y-%m-%d %H:%M:%S", tz="")), 
+                         format = "%H")
+
+songs$Min_Stamp = format(as.POSIXct(strptime(songs$Timestamp, "%Y-%m-%d %H:%M:%S", tz="")), 
+                         format = "%M")
+
+songs$Sec_Stamp = format(as.POSIXct(strptime(songs$Timestamp, "%Y-%m-%d %H:%M:%S", tz="")), 
+                         format = "%S")
+
+attach(songs)
+
+songs = songs[ ,-6]
+
+# Convert Char into numbers
+
+songs$Day_Stamp = as.numeric(songs$Day_Stamp)
+songs$Month_Stamp = as.numeric(songs$Month_Stamp)
+songs$Year_Stamp = as.numeric(songs$Year_Stamp)
+songs$Hour_Stamp = as.numeric(songs$Hour_Stamp)
+songs$Min_Stamp = as.numeric(songs$Min_Stamp)
+songs$Sec_Stamp = as.numeric(songs$Sec_Stamp)
+
+## Origin Artist country is same - Remove country feature
+
+songs = songs[, -4]
+attach(songs)
+
+
+## Correlation Study 1: Views, Comments, Likes, Popularity, Followers
+## Correlation Study 2: Views, Day, Month, Year, Hour, Min, Sec
+
+library(ggcorrplot)
+library(corrplot)
+library(RColorBrewer)
+
+
+Corr1 = corrplot(cor(songs[, c(5,6,7,8,9)]), type = 'upper', order = 'hclust',
+                 col = brewer.pal(n=5, name = 'YlGnBu')) 
+
+Corr2 = corrplot(cor(songs[, c(5,10,11,12,13,14,15)]), type = 'upper', order = 'hclust',
+                col = brewer.pal(n=7, name = 'YlOrBr')) 
+
+
+## Pivot table analysis
+
+library(rpivotTable)
+
+rpivotTable(songs)
+
+##
+# Highest Likes to Names: LIL UZI VERT, Kodak Black and Future 
+# Highest Likes to Genre: all Music, danceedm 
+# Highest Likes in a Year: 2025 2016 2017
+# Highest Likes in a month: March and December
+# Highest Likes on a day: 15th, 16th, 17th, 26th
+# Highest Likes for an hour: 0th hour Mindnight - 18th, 19th, 22nd (Evening)
+# Highest Likes for a min: Starting of an hour 0,1,2,3,etc. 
+# Highest Likes for a sec: Starting of a sec 0,1,2,3,etc.
+
+
+## Outliers Study
+
+
+boxplot(Comments, main = 'Comments', col = 'coral')
+boxplot(Likes, main = 'Likes', col = 'coral')
+boxplot(Popularity, main = 'Popularity', col = 'coral')
+boxplot(Followers, main = 'Followers', col = 'coral')
+
+
+## Data Discribution : Histogram
+
+hist(Followers)
+
+## Remove Extreem Outlier from Comments Feature
+
+songs = songs[-54240,]
+attach(songs)
+
+## NOTES:
+# Oultiers exisits in Comments and Followers
+# Due to High Correlation between Views and Comments we decided not to remove Outliers.
+# But We removed one extreem outlier. 
+# Due to Low correlation between Views and Followers 
+# We analysized Highest followers are with Future - and that is the outliers. But can't remove the Genre 
+
+
+## Plot regression charts
+
+plot(Comments, Likes, main = 'Comments & Likes', col  = 'coral', abline(lm(Likes~Comments)))
+plot(Popularity, Likes, main = 'Popularity & Likes', col  = 'coral', abline(lm(Likes~Popularity)))
+
+
+write.csv(songs, 'songs_clean.csv')
+
+
+
+
+
+
